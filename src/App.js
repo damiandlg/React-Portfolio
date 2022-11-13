4d6325db84a79cb0deb4df954d5b2e1da889c769
damiandlg <damian.delagarzaf@gmail.com>
2022-11-12 19:48:47 -0600

    half way done with Nav menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NavMenu from './components/NavMenu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Project from './pages/Project';
 
 
-function App() {
+export default function App () {
   return (
-    <div>
-
-    <p>Hello World !</p>
-    </div>
+    <>
+    <Router>
+    <NavMenu />
+      <Home></Home>
+      <About></About>
+      <Project></Project>
+      <Contact></Contact>
+    </Router>
+    </>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.js';
+import GlobalStyles from './styles/GlobalStyle';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const head = ReactDOM.createRoot(document.querySelector('head'));
-
-head.render(
+ReactDOM.render(
   <>
-        <meta charset="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Damian De La Garza Portfolio</title>
-        <link rel ="stylesheet" href="assets/css/style.css" />
-        </>
-);
-root.render(
-  
+    <GlobalStyles />
     <App />
+  </>
+  //document.getElementById('root')
 );
+
 
